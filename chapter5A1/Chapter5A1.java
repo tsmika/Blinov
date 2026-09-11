17cb7dd53a18a45a8cfdd23f5fe25f994e3df512
tsmika <tsikhankaras@gmail.com>
2026-09-11 15:17:44 +0300

    solved: chapter 5 -> var A -> task 1

---
+++ chapter5A1/Chapter5A1.java
@@ -0,0 +1,51 @@
+package chapter5A1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import chapter5A1.Notepad.Note;
+
+/*
+Создать класс Notepad с внутренним классом или классами, с помощью
+объектов которого могут храниться несколько записей на одну дату.
+ */
+
+public class Chapter5A1 {
+
+	public static void main(String[] args) {
+		Notepad notepad = getTestData();
+		System.out.println(notepad.getAllNotes());
+	}
+
+	private static Notepad getTestData() {
+
+		List<Note> notes = new ArrayList<>();
+		Notepad notepad = new Notepad(notes);
+		List<String> records1 = new ArrayList<>();
+		List<String> records2 = new ArrayList<>();
+		List<String> records3 = new ArrayList<>();
+
+		records1.add("Позвонить клиенту1");
+		records1.add("Отправить отчет1");
+		records1.add("Проверить сервер1");
+		Notepad.Note note1 = notepad.new Note(records1, LocalDate.of(2026, 2, 8));
+
+		records2.add("Позвонить клиенту2");
+		records2.add("Отправить отчет2");
+		records2.add("Проверить сервер2");
+		Note note2 = notepad.new Note(records2, LocalDate.of(2026, 7, 15));
+
+		records3.add("Позвонить клиенту3");
+		records3.add("Отправить отчет3");
+		records3.add("Проверить сервер3");
+		Note note3 = notepad.new Note(records3, LocalDate.now());
+
+		notes.add(note1);
+		notes.add(note2);
+		notes.add(note3);
+
+		return notepad;
+	}
+
+}
---
+++ chapter5A1/Notepad.java
@@ -0,0 +1,42 @@
+package chapter5A1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notepad {
+
+	private final List<Note> notes;
+
+	public Notepad(List<Note> notes) {
+		this.notes = notes;
+	}
+
+	public void addNote(Note note) {
+		notes.add(note);
+	}
+
+	public String getAllNotes() {
+
+		StringBuilder builder = new StringBuilder();
+
+		for (Note note : notes) {
+			builder.append(note.date.toString()).append(" :\n");
+			for (String item : note.records) {
+				builder.append(item + "\n");
+			}
+		}
+
+		return builder.toString();
+	}
+
+	public class Note {
+		private final LocalDate date;
+		private final List<String> records;
+
+		public Note(List<String> records, LocalDate date) {
+			this.date = date;
+			this.records = new ArrayList<>(records);
+		}
+	}
+}
